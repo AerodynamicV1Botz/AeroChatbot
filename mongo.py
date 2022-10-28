817772efc3330619b9269792db3ae9371669cdab
AerodynamicV1Botz <104615877+AerodynamicV1Botz@users.noreply.github.com>
2022-10-28 12:12:44 +0530

    Update mongo.py
    Signed-off-by: AerodynamicV1Botz <104615877+AerodynamicV1Botz@users.noreply.github.com>

--- mongo.py
+++ mongo.py
@@ -1,9 +1,9 @@
 #
-# Copyright (C) 2021-2022 by TeamYukki@Github, < https://github.com/YukkiChatBot >.
+# Copyright (C) 2021-2022 by AerodynamicV1Botz@Github, < https://github.com/YukkiChatBot >.
 #
-# This file is part of < https://github.com/TeamYukki/YukkiChatBot > project,
+# This file is part of < https://github.com/AerodynamicV1Botz/AeroChatbot > project,
 # and is released under the "GNU v3.0 License Agreement".
-# Please see < https://github.com/TeamYukki/YukkiChatBot/blob/master/LICENSE >
+# Please see < https://github.com/AerodynamicV1Botz/AeroChatbot/blob/master/LICENSE >
 #
 # All rights reserved.
 #
